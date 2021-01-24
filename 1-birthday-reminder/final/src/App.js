a54d2026fc187e858d4a1c7cbdae14e7a4b7f024
Gal Gorelik <jenya_g@msn.com>
2021-01-24 15:48:17 +0200

    new proj

---
+++ 1-birthday-reminder/final/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  const [people, setPeople] = useState(data);
+  const [btn, setBtn] = useState(true);
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        {btn ? <button onClick={() => {
+          return setPeople([]), setBtn(false)
+        }}>clear all</button> :
+          <button onClick={() => {
+            return (
+              setPeople(data), setBtn(true)
+            )
+          }}
+          >refresh</button>
+        }
+
+
+      </section>
+    </main>
+  );
+}
+
+export default App;
